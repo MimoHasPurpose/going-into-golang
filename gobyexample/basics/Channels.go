dbf406106c5af93d1bf8a4d4c01802f64e399a6c
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-04-15 12:29:43 +0530

    select

--- gobyexample/basics/Channels.go
+++ gobyexample/basics/Channels.go
@@ -5,8 +5,13 @@ package main
 import "fmt"
 func main(){
 	messages:=make(chan string)		// create a new channel by make.
+
+	tommy:=make(chan string)
+	go func(){tommy<-"hello tommy"}()
 	go func(){ messages<- "ping"}()
 
 	msg:=<-messages
+	tom:=<-tommy
+	fmt.Println(tom)
 	fmt.Println(msg)
 }
--- gobyexample/basics/goroutines.go
+++ gobyexample/basics/goroutines.go
@@ -7,21 +7,27 @@ import(
 )
 
 func f(from string){
-	for i:=0;i<3;i++{
+	for i:=0;i<30;i++{
 		fmt.Println(from,":",i)
+	}
+}
+func ff(from string){
+	for i:=0;i<1;i++{
+		fmt.Println(from,": ",i)
 	}
 }
 
 func main(){
 	f("direct")
-	 
-	go f("goroutine")
+	
+	time.Sleep(time.Second)
+	go ff("goroutine")
 
 	go func(msg string){
 		fmt.Println(msg)
 
 	}("going")
-
-	time.Sleep(time.Second)
+	// time.Sleep(time.Second)
+	// time.Sleep(time.Second)
 	fmt.Println("done")
 }
