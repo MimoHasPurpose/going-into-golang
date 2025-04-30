e18d95b8a1b79b4f255dea24241ec3660677e587
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-04-30 17:27:06 +0530

    updat

--- gobyexample/basics/part-four/commandlinesubcommands.go
+++ gobyexample/basics/part-four/commandlinesubcommands.go
@@ -7,5 +7,8 @@ import (
 )
 
 func main(){
-	
+	fooCmd:=flag.NewFlagSet("foo",flag.ExitOnError)
+	fooEnable:=fooCmd.Bool("enable",false,"enable")
+	fooName:=fooCmd.String("name","","name")
+	barCmd:=flag.NewFlagSet("bar",flag.ExitOnError)
 }
