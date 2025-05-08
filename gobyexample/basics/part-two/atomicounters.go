94f5cb13e2f81416a86c6ec5516b1c8ef23e563d
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-05-08 19:46:51 +0530

    update

--- gobyexample/basics/part-two/atomicounters.go
+++ gobyexample/basics/part-two/atomicounters.go
@@ -1,1 +1,35 @@
-// sync/atomic package for state management.
+// primary mechanisms for managing state in go is communicating over channels.
+// 
+// 
+
+package main
+
+import (
+    "fmt"
+    "sync"
+    "sync/atomic"
+)
+
+func main() {
+
+    var ops atomic.Uint64
+
+    var wg sync.WaitGroup
+
+    for i := 0; i < 50; i++ {
+        wg.Add(1)
+
+        go func() {
+            for c := 0; c < 1000; c++ {
+
+                ops.Add(1)
+            }
+
+            wg.Done()
+        }()
+    }
+
+    wg.Wait()
+
+    fmt.Println("ops:", ops.Load())
+}
--- gobyexample/basics/part-two/nonblockingchanneloperations.go
+++ gobyexample/basics/part-two/nonblockingchanneloperations.go
@@ -1,3 +1,31 @@
 // basic sends and receives on channls are blocking
 // we can use 'select' with a default clause  to implement 'non-blocking' sends, reeives, and even non 
 // blocking multi-way selects.
+package main
+import "fmt"
+func main(){
+	messages:=make(chan string)
+	signals:=make(chan bool)
+	select{
+	case msg:=<-messages:
+		fmt.Println("received message",msg)
+	default:
+		fmt.Println("no message received")
+	}
+	msg:="hi"
+	select{
+	case messages<-msg:
+		fmt.Println("sent messag",msg)
+	default:
+		fmt.Println("no message sent")
+	}
+	select{
+	case msg:=<-messages:
+		fmt.Println("received message",msg)
+	case sig:=<-signals:
+		fmt.Println("received signal",sig)
+	default:
+		fmt.Println("no activity")
+
+	}
+}
